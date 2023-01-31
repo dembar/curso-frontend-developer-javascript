c78f95428abb49d338ae76e9f6747dcb74636847
Daniel Masias <danielmasias4@gmail.com>
2023-01-30 20:15:38 -0500

    carrito, menumobile, producDetail funcionando mobile y desktop

--- main.js
+++ main.js
@@ -2,14 +2,18 @@ const menuEmail =document.querySelector('.navbar-email');
 const menuHamIcon =document.querySelector('.menu');
 const menuCarritoIcon =document.querySelector('.navbar-shopping-cart');
 const desktopMenu =document.querySelector('.desktop-menu');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const mobileMenu =document.querySelector('.mobile-menu');
 const shoppingCartcontainer = document.querySelector('#shoppingCartcontainer');
+const producDetailContainer = document.querySelector('#productDetail');
+
 const cardsContainer = document.querySelector('.cards-container');
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
-menuHamIcon.addEventListener('click', togglemobileMenu);
-menuCarritoIcon.addEventListener('click', toggleCarritoshoppingCartcontainer);
 
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu(){
     const isshoppingCartcontainerClosed = shoppingCartcontainer.classList.contains('inactive');
@@ -21,7 +25,8 @@ function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
 
-function togglemobileMenu(){
+function toggleMobileMenu(){
+    producDetailContainer.classList.add('inactive');
     const isshoppingCartcontainerClosed = shoppingCartcontainer.classList.contains('inactive');
 
     if(!isshoppingCartcontainerClosed){
@@ -30,7 +35,8 @@ function togglemobileMenu(){
     mobileMenu.classList.toggle('inactive');
 }
 
-function toggleCarritoshoppingCartcontainer(){
+function toggleCarritoAside(){
+    
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
     
     //const isshoppingCartcontainerClosed = shoppingCartcontainer.classList.contains('inactive');
@@ -39,9 +45,26 @@ function toggleCarritoshoppingCartcontainer(){
         mobileMenu.classList.add('inactive');
     }
 
+    const isProductDetailClosed = producDetailContainer.classList.contains('inactive');
+    
+    //const isshoppingCartcontainerClosed = shoppingCartcontainer.classList.contains('inactive');
+
+    if(!isProductDetailClosed){
+        producDetailContainer.classList.add('inactive');
+    }
+
     shoppingCartcontainer.classList.toggle('inactive');
 }
 
+function openProductDetailAside(){
+  producDetailContainer.classList.remove('inactive');
+  shoppingCartcontainer.classList.add('inactive');
+}
+
+function closeProductDetailAside(){
+  producDetailContainer.classList.add('inactive');
+
+}
 const productList = [];
 productList.push({
     name: 'Bike',
@@ -61,31 +84,14 @@ productList.push({
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 
-
-/*<section class="main-container">
-    <div class="cards-container">
-
-      <div class="product-card">
-        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-        <div class="product-info">
-          <div>
-            <p>$120,00</p>
-            <p>Bike</p>
-          </div>
-          <figure>
-            <img src="./icons/bt_add_to_cart.svg" alt="">
-          </figure>
-        </div>
-      </div>
-    </div>
-  </section>*/
-
-for (product of productList){
+function renderProducts(arr){
+  for (product of arr){
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
 
     const productImg =  document.createElement('img');
     productImg.setAttribute('src', product.image);
+    productImg.addEventListener('click', openProductDetailAside);
 
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');   
@@ -115,4 +121,7 @@ for (product of productList){
     cardsContainer.appendChild(productCard);
 
     //product = {name, price, image}
+  }
 }
+
+renderProducts(productList);
