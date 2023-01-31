4892624385d98427b67d0846d4a96ec25c70acdf
Daniel Masias <danielmasias4@gmail.com>
2023-01-30 19:03:29 -0500

    Añadiendo la parte de detalle del producto

--- main.js
+++ main.js
@@ -3,64 +3,46 @@ const menuHamIcon =document.querySelector('.menu');
 const menuCarritoIcon =document.querySelector('.navbar-shopping-cart');
 const desktopMenu =document.querySelector('.desktop-menu');
 const mobileMenu =document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartcontainer = document.querySelector('#shoppingCartcontainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', togglemobileMenu);
-menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+menuCarritoIcon.addEventListener('click', toggleCarritoshoppingCartcontainer);
 
 
 function toggleDesktopMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isshoppingCartcontainerClosed = shoppingCartcontainer.classList.contains('inactive');
     
-    if(!isAsideClosed){
-        aside.classList.add('inactive');
+    if(!isshoppingCartcontainerClosed){
+        shoppingCartcontainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
 }
 
 function togglemobileMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isshoppingCartcontainerClosed = shoppingCartcontainer.classList.contains('inactive');
 
-    if(!isAsideClosed){
-        aside.classList.add('inactive');
+    if(!isshoppingCartcontainerClosed){
+        shoppingCartcontainer.classList.add('inactive');
     }
     mobileMenu.classList.toggle('inactive');
 }
 
-function toggleCarritoAside(){
+function toggleCarritoshoppingCartcontainer(){
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
     
-    //const isAsideClosed = aside.classList.contains('inactive');
+    //const isshoppingCartcontainerClosed = shoppingCartcontainer.classList.contains('inactive');
 
     if(!isMobileMenuClosed){
         mobileMenu.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive');
+    shoppingCartcontainer.classList.toggle('inactive');
 }
 
 const productList = [];
-productList.push({
-    name: 'Bike',
-    price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-});
-
-productList.push({
-    name: 'Pantalla',
-    price: 220,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-});
-
-productList.push({
-    name: 'compu',
-    price: 520,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-});
-
 productList.push({
     name: 'Bike',
     price: 120,
